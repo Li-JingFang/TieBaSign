b73699793324709c5d71d8f08ff65882db344b62
JingFang Li <104253368+Li-JingFang@users.noreply.github.com>
2025-12-05 17:54:28 +0800

    debug1

--- main.py
+++ main.py
@@ -180,6 +180,7 @@ def main():
         favorites = get_favorite(i)
         for j in favorites:
             time.sleep(random.randint(1,5))
+            logger.info(type(j))
             client_sign(i, tbs, j["id"], j["name"])
         logger.info("完成第" + str(n) + "个用户签到")
     logger.info("所有用户签到结束")
